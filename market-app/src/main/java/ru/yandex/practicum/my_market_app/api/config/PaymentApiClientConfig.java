084ede18ceacef431a6e497b5122f861590e24f4
Sergey.Kolotevsergey.kolotev@glowbyteconsulting.com <you@example.com>
2026-06-13 20:34:01 +0300

    feature: add PaymentService timeouts

--- market-app/src/main/java/ru/yandex/practicum/my_market_app/api/config/PaymentApiClientConfig.java
+++ market-app/src/main/java/ru/yandex/practicum/my_market_app/api/config/PaymentApiClientConfig.java
@@ -13,12 +13,17 @@
 
 package ru.yandex.practicum.my_market_app.api.config;
 
+import io.netty.channel.ChannelOption;
+import reactor.netty.http.client.HttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
 
 @Configuration
 public class PaymentApiClientConfig {
@@ -26,10 +31,16 @@ public class PaymentApiClientConfig {
     @Value("${payment-service.base-url:http://localhost:8090}")
     private String baseUrl;
 
+
     @Bean
     public WebClient getWebClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3_000)
+                .responseTimeout(Duration.ofSeconds(3));
+
         return WebClient.builder()
                 .baseUrl(this.baseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
